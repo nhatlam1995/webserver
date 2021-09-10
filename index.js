a68f039119d5a6ede7a9393bdf7d5dc5e215bfcd
nhatlam1995 <nhatlam1695@gmail.com>
2021-09-10 18:00:52 +0700

    d

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
