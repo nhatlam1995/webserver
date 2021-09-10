365ce0938dbc14e0f7b2e93778d6fe20ccfcc544
nhatlam1995 <nhatlam1695@gmail.com>
2021-09-10 17:03:17 +0700

    Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const authRouter = require('./routes/auth.js')
+const postRouter = require('./routes/post')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.DB_LINK}`)
+
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
+
+const PORT = 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
